5dfa48193f11c15fa58b5ec08cc886354cad0f3e
shakyapeiris <shakyaimanjith32@gmail.com>
2022-08-05 23:51:24 +0530

    Fix issues with signup page

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -1,14 +1,14 @@
 import SignUpForm from "../Components/SignUpForm";
 
 const SignUp = () => (
-  <div className="flex w-full">
-    <div className="flex flex-col md:w-5/12 w-full lg:px-[40px] px-[20px] justify-evenly items-start h-[100vh]">
-      <img src="/logo.png" alt="ubetatta" className="w-60" />
-      <p className="text-[3rem] font-black  w-full text-left">Sign Up</p>
+  <div className="flex w-full items-end justify-between lg:items-center h-[100vh]">
+    <div className="custom-scroll-bar md:w-[49.7%] w-full lg:px-[40px] md:px-[20px] sm:px-[10%]  px-[20px] overflow-y-scroll h-[100vh]">
+      <img src="/logo.png" alt="logo" className="md:mt-[45px] md:w-[200px] sm:w-[175px] w-[125px] relative left-[50%] translate-x-[-50%] mt-[25px] md:static md:left-0 md:translate-x-0 md:top-0" />
+      <p className="my-[35px] text-[2rem] font-black  w-full text-left sm:text-center md:text-left text-[#1E1E1E]">Sign Up</p>
       <SignUpForm />
     </div>
 
-    <div className="w-7/12 h-[100vh] bg-[url('https://images.unsplash.com/photo-1583088580009-2d947c3e90a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80')] bg-cover bg-center hidden md:block" />
+    <div className="w-6/12 h-[100vh] bg-[url('https://images.unsplash.com/photo-1583088580009-2d947c3e90a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80')] bg-cover bg-center hidden md:block" />
   </div>
 );
 
