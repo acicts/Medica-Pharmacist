362e8f216905d584a24285d456e40bd2f7eec757
Tharindu J <tharindujayakody21@gmail.com>
2022-09-18 12:40:52 +0530

    Medicine Listing Page
    Installed packages
     -react-table
     -@types/react-table

--- src/Pages/Medicine.tsx
+++ src/Pages/Medicine.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import { AiOutlinePlus } from 'react-icons/ai';
+import MedTable from '../Components/Table';
+
 
 const Medicine = () => {
-	return <div>Medicine</div>;
-};
+    return (
+      <div>
+    <div className="flex md:flex md:items-center md:justify-between">
+      <div className="sm:min-w-0 flex-1">
+        <h1 className="text-2xl font-bold sm:text-2xl width-auto">
+          List of Medicines
+        </h1>
+         <div className="row-span-1">
+            <p className="text-xs">
+          List of Medicines Available
+            </p>
+            </div>
+        </div>
+    
+   
+
+      <div className="mt-5 flex lg:mt-0 lg:ml-4">
+
+        <span className="sm:ml-3">
+    
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-[#152927] focus:outline-none"
+          >
+            <AiOutlinePlus className="h-5 w-5 flex-shrink-0 text-white" aria-hidden="true" />
+            <p className="sm:flex hidden ml-1.5">Add New Item</p>
+          </button>
+        </span>
+
+      </div>
+
+</div>
+
+        <MedTable placeholder="Search Inventory.." />
+
+      </div>
+    
+   ) 
+}
 
 export default Medicine;
+
+
+
